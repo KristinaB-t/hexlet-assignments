3752df3c9953c8e48d0c8f68578941c48a98c117
@hexlet/cli <support@hexlet.io>
2025-01-20 22:06:17 +0300

    submit java-web-ru/handlers

---
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -0,0 +1,32 @@
+package exercise;
+
+import io.javalin.Javalin;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        // BEGIN
+                // Создаем приложение
+        var app = Javalin.create(config -> {
+            // Включаем логгирование при разработке
+            config.bundledPlugins.enableDevLogging();
+        });
+
+	List<String> phones = Data.getPhones();
+	List<String> domains = Data.getDomains();
+        // Описываем, что будет происходить при GET запросе на адрес /
+        // Метод json() кодирует тело ответа в JSON строку и вызывает метод result()
+        // Дополнительно устанавливает в ответе заголовок content type со значением json
+        app.get("GET /phones", ctx -> ctx.json(phones));
+        app.get("GET /domains", ctx -> ctx.json(domains));
+        // Возвращаем настроенное приложение
+        return app;        
+        // END
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
